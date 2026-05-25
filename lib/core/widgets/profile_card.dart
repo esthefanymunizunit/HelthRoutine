4bf3dc42f4a094ece1aeaf5707b6fc8f14073977
fabiotarcisio <fabio.tarcisio@souunit.com.br>
2026-05-25 13:02:33 -0300

    [ADD] Widget profile card

---
+++ lib/core/widgets/profile_card.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:healthroutine/core/theme/app_colors.dart';
+
+class ProfileSectionCard extends StatelessWidget {
+  final String title;
+  final List<Widget> children;
+  final bool showEditButton;
+
+  const ProfileSectionCard({
+    super.key, 
+    required this.title, 
+    required this.children, 
+    this.showEditButton = false
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      clipBehavior: Clip.none,
+      alignment: Alignment.bottomCenter,
+      children: [
+        Container(
+          padding: const EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(30),
+            border: Border.all(color: AppColors.cloudBlue.withValues(alpha: 0.3)),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(title, style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.grey)),
+              const SizedBox(height: 15),
+              ...children,
+            ],
+          ),
+        ),
+        if (showEditButton)
+          Positioned(
+            bottom: -20,
+            child: FloatingActionButton.small(
+              onPressed: () {},
+              backgroundColor: AppColors.cloudBlue,
+              child: const Icon(Icons.edit, color: Colors.white),
+            ),
+          ),
+      ],
+    );
+  }
+}
